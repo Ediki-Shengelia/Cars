bf408431d53b08c6bd2efaa08c1a56c2eeb1e907
ediki <shengelia301@gmail.com>
2024-05-06 15:57:55 +0400

    bar

--- bar.js
+++ bar.js
@@ -1,4 +1,22 @@
-document.getElementById('burger').addEventListener('click', function() {
-  this.classList.toggle('open');
-  document.querySelector('.nav-menu').classList.toggle('open');
+document.getElementById("burger").addEventListener("click", function () {
+  this.classList.toggle("open");
+  document.querySelector(".nav-menu").classList.toggle("open");
+});
+
+// scroll to top
+
+let topIcon = document.getElementById("top");
+topIcon.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 700) {
+    topIcon.classList.add("newIcon");
+  } else {
+    topIcon.classList.remove("newIcon");
+  }
 });

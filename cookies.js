03423b29d97a2d4e02d24cc70e6df72797b3cc54
ediki <shengelia301@gmail.com>
2024-05-08 19:24:47 +0400

    cookies.js

--- cookies.js
+++ cookies.js
@@ -1,20 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cookieNotification = document.getElementById("cookie-notification");
-    var acceptCookiesButton = document.getElementById("accept-cookies");
+"use strict";
 
-    // Check if user has already accepted cookies
-    var cookiesAccepted = localStorage.getItem("cookiesAccepted");
+document.addEventListener("DOMContentLoaded", function () {
+  var cookieNotification = document.getElementById("cookie-notification");
+  var acceptCookiesButton = document.getElementById("accept-cookies");
 
-    if (!cookiesAccepted) {
-        // Show the cookies notification
-        cookieNotification.style.display = "block";
-    }
+  // Check if user has already accepted cookies
+  var cookiesAccepted = localStorage.getItem("cookiesAccepted");
 
-    // Event listener for accept cookies button
-    acceptCookiesButton.addEventListener("click", function() {
-        // Hide the cookies notification
-        cookieNotification.style.display = "none";
-        // Set a flag in localStorage to remember that the user accepted cookies
-        localStorage.setItem("cookiesAccepted", true);
-    });
+  if (!cookiesAccepted) {
+    // Show the cookies notification
+    cookieNotification.style.display = "block";
+  }
+
+  // Event listener for accept cookies button
+  acceptCookiesButton.addEventListener("click", function () {
+    // Hide the cookies notification
+    cookieNotification.style.display = "none";
+    // Set a flag in localStorage to remember that the user accepted cookies
+    localStorage.setItem("cookiesAccepted", true);
+  });
 });
+function phone() {
+ 
+  document.getElementById("userNumber").addEventListener("submit", function (e) {
+    
+    e.preventDefault();
+    
+    
+    let checkbox = document.getElementById("save");
+    let inputValue = document.getElementById("phone").value;
+
+    
+    if (checkbox.checked) {
+    
+      Cookies.set("cookiesUserNumber", inputValue);
+    } else {
+    
+      Cookies.remove("cookiesUserNumber");
+    }
+  });
+
+ 
+  let savedUserNumber = Cookies.get("cookiesUserNumber");
+  if (savedUserNumber) {
+
+    document.getElementById("phone").value = savedUserNumber;
+
+    document.getElementById("save").checked = true;
+  }
+}
+
+
+phone();
